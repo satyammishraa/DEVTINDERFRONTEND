93c4d0fbb96b46255949a72be9a7b208a52799bc
satyammishraa <163456205+satyammishraa@users.noreply.github.com>
2025-12-08 10:50:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Force classic JSX runtime so React is always imported
 export default defineConfig({
   plugins: [
     react({

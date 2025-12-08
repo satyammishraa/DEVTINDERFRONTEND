d93c9c488e72b684b5199e6f2b751f54ccd50ec8
satyammishraa <163456205+satyammishraa@users.noreply.github.com>
2025-12-08 15:40:32 +0530

    Update Premium.jsx

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,32 +1,7 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { BASE_URL } from "../utils/constants";
+import React from "react";
 
 const Premium = () => {
-  const [isUserPremium, setIsUserPremium] = useState(false);
-
-  useEffect(() => {
-    verifyPremiumUser();
-  }, []);
-
-  const verifyPremiumUser = async () => {
-    try {
-      const res = await axios.get(BASE_URL + "/premium/verify", {
-        withCredentials: true,
-      });
-      if (res.data.isPremium) {
-        setIsUserPremium(true);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  return isUserPremium ? (
-    <h1 className="text-center mt-10 text-2xl">
-      You are already a premium user.
-    </h1>
-  ) : (
+  return (
     <div className="m-10 text-center text-xl">
       <h1 className="mb-4 text-3xl font-bold">Premium Feature Coming Soon</h1>
       <p>This feature is currently disabled.</p>

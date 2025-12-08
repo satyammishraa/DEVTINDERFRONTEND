440f6306d48d3a0af3d2eb1eec713d28c9216350
satyammishraa <163456205+satyammishraa@users.noreply.github.com>
2025-12-08 11:13:40 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,16 +2,8 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    // local → local backend
-    return io(BASE_URL, {
-      withCredentials: true,
-    });
-  } else {
-    // production → connect to Render backend
-    return io(import.meta.env.VITE_BACKEND_URL, {
-      withCredentials: true,
-      transports: ["websocket"], 
-    });
-  }
+  return io(BASE_URL, {
+    withCredentials: true,
+    transports: ["websocket"],
+  });
 };
